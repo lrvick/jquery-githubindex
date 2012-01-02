570d751b8ae91bdf7ac48205583f13accfa3ba7a
lrvick <lance@lrvick.net>
2012-01-02 08:32:19 -0500

    first commit

---
+++ jquery.githubindex.js
@@ -0,0 +1,95 @@
+(function($) {
+    $.fn.githubIndex = function(users, repos) {
+        var $el = this
+        var feed = [];
+
+        var getRepo = function() {
+            var parts = repos.pop().split('/')
+            var user = parts[0]
+            var repo = parts[1]
+            var repo_url = 'https://api.github.com/repos/' + user + '/' + repo + '?callback=?'
+            $.getJSON(repo_url, function(data) {
+                feed.push(data.data)
+                getFeeds();
+            })
+        }
+
+        var getUserRepos = function() {
+            var user = users.pop();
+            var user_url = 'https://api.github.com/users/' + user + '/repos?per_page=100&type=owner&callback=?'
+            $.getJSON(user_url, function(data) {
+                $.each(data.data, function(key, value) {
+                    if (value['fork'] === false) {
+                        feed.push(value)
+                    }
+                })
+                getFeeds()
+            })
+        }
+
+        var sortFeeds = function(feed) {
+            feed.sort(function(a, b) {
+                var date_a = new Date(a['updated_at'])
+                var date_b = new Date(b['updated_at'])
+                return date_b.getTime() - date_a.getTime();
+            });
+            return feed
+        }
+
+        var getFeeds = function() {
+            if (repos.length > 0) {
+                getRepo();
+            } else if (users.length > 0) {
+                getUserRepos();
+            } else {
+                feed = sortFeeds(feed);
+                $.each(feed, function(key, value) {
+                    var $article = $('<article>').appendTo($el)
+                    var $h2 = $('<h2>').appendTo($article)
+                    $('<a>')
+                        .attr('href', value['html_url'])
+                        .text(value['name'])
+                        .appendTo($h2)
+                    $('<p>')
+                        .addClass('description')
+                        .text(value['description'])
+                        .appendTo($article)
+                    var $dl = $('<dl>').appendTo($article)
+                    $('<dt>')
+                        .text('Last Updated:')
+                        .appendTo($dl)
+                    $('<dl>')
+                        .text(value['updated_at'])
+                        .appendTo($dl)
+                    if (value['language']){
+                        $('<dt>')
+                            .text('Primary Language:')
+                            .appendTo($dl)
+                    $('<dl>')
+                        .text(value['language'])
+                        .appendTo($dl)
+                    }
+                $('<dt>')
+                    .text('Lines:')
+                    .appendTo($dl)
+                $('<dl>')
+                    .text(value['size'])
+                    .appendTo($dl)
+                $('<dt>')
+                    .text('Repo URL:')
+                    .appendTo($dl)
+                var $repo_dl = $('<dl>')
+                    .appendTo($dl)
+                $('<a>')
+                    .attr('href', value['html_url'])
+                    .text(value['html_url'])
+                    .appendTo($repo_dl)
+                })
+            }
+        }
+
+        getFeeds();
+
+    }
+})(jQuery);
+
